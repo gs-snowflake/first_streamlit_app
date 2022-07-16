5ee775dac1da8384d784ee5454bf94bdc2654133
gs-snowflake <109354406+gs-snowflake@users.noreply.github.com>
2022-07-16 08:33:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
+streamlit.title('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
