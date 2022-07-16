9db0998dd8aba75ca770e0e19edb05ac410fc8d4
gs-snowflake <109354406+gs-snowflake@users.noreply.github.com>
2022-07-16 08:34:51 +0100

    Create another header for a special menu item.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ streamlit.title('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
